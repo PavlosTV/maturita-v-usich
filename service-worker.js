9345f92656dd466902289bf71ed96a65b9d59fba
PavlosTV <pvaclavek@cmgp.cz>
2026-09-15 00:03:43 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "maturita-v-usich-v2";
+const CACHE_NAME = "maturita-v-usich-v3";
 
 const FILES_TO_CACHE = [
     "./",
@@ -29,8 +29,18 @@ self.addEventListener("activate", event => {
 
 self.addEventListener("fetch", event => {
     event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request);
-        })
+        fetch(event.request)
+            .then(response => {
+                const responseClone = response.clone();
+
+                caches.open(CACHE_NAME).then(cache => {
+                    cache.put(event.request, responseClone);
+                });
+
+                return response;
+            })
+            .catch(() => {
+                return caches.match(event.request);
+            })
     );
 });
