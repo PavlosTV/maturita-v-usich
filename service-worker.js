488ee0ea23e408c102e6b127a08a9e1f7eb12f29
PavlosTV <pvaclavek@cmgp.cz>
2026-09-15 18:28:16 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,17 +2,23 @@ const CACHE_NAME = "maturita-v-usich-v4";
 
 const FILES_TO_CACHE = [
     "./",
-    "./index.html?v2",
-    "./style.css?v2",
-    "./app.js?v2",
-    "./manifest.json?v2",
+    "./index.html",
+    "./style.css",
+    "./app.js",
+    "./manifest.json",
 
+    "./data/categories.json",
+    "./data/periods.json",
     "./data/songs.json",
     "./data/stredovek.json",
 
     "./audio/stredovek.mp3"
 ];
 
+
+// ========================================
+// INSTALACE NOVÉ VERZE
+// ========================================
 
 self.addEventListener("install", event => {
 
@@ -26,22 +32,72 @@ self.addEventListener("install", event => {
                 );
 
             })
+            .then(() => {
+
+                // Nová verze se aktivuje okamžitě
+
+                return self.skipWaiting();
+
+            })
 
     );
 
 });
 
 
+// ========================================
+// AKTIVACE
+// ========================================
+
+self.addEventListener("activate", event => {
+
+    event.waitUntil(
+
+        caches.keys()
+            .then(cacheNames => {
+
+                return Promise.all(
+
+                    cacheNames
+                        .filter(name =>
+                            name !== CACHE_NAME
+                        )
+                        .map(name =>
+                            caches.delete(name)
+                        )
+
+                );
+
+            })
+            .then(() => {
+
+                // Nový Service Worker začne
+                // okamžitě ovládat stránku
+
+                return self.clients.claim();
+
+            })
+
+    );
+
+});
+
+
+// ========================================
+// NAČÍTÁNÍ SOUBORŮ
+// ========================================
+
 self.addEventListener("fetch", event => {
 
     event.respondWith(
 
         caches.match(event.request)
-            .then(response => {
+            .then(cachedResponse => {
 
-                if (response) {
-                    return response;
+                if (cachedResponse) {
+                    return cachedResponse;
                 }
+
 
                 return fetch(event.request);
 
