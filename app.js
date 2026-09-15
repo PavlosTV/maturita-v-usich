7ecaef182d6fcff8928f87456bc4723b3e08b9e9
PavlosTV <pvaclavek@cmgp.cz>
2026-09-15 11:34:59 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
+const songs = [];
 
+async function loadSongs() {
+
+    const response =
+        await fetch("./data/songs.json");
+
+    const data =
+        await response.json();
+
+    songs.push(...data);
+
+    console.log("Načtené skladby:", songs);
+}
+
+loadSongs();
