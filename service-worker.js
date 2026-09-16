da59f797867c8380e89dcc45b059d0418b2126a5
PavlosTV <pvaclavek@cmgp.cz>
2026-09-16 10:02:18 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -91,3 +91,15 @@ self.addEventListener("fetch", event => {
     );
 
 });
+self.addEventListener("message", event => {
+
+    if (
+        event.data &&
+        event.data.type === "SKIP_WAITING"
+    ) {
+
+        self.skipWaiting();
+
+    }
+
+});
