fd9dd9c14b3973da745905cf307d26fca08f7b98
PavlosTV <pvaclavek@cmgp.cz>
2026-09-20 15:44:36 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
 // SERVICE WORKER - MATURITA V UŠÍCH
 // ========================================
 
-const APP_CACHE = "maturita-v-usich-app-v9"; // Zvýšili jsme verzi
+const APP_CACHE = "maturita-v-usich-app-v10"; // Zvýšili jsme verzi
 const CONTENT_CACHE = "maturita-v-usich-content-v1"; // Tuto schránku budeme chránit
 
 // Seznam chráněných schránek (tyto se nesmí smazat)
